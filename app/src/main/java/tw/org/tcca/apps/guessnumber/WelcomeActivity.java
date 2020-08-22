df671ce0fff9bcf26b52eeaf693f089d56319bc4
bradchao <brad@brad.tw>
2020-08-22 14:08:09 +0800

    202008221408

--- app/src/main/java/tw/org/tcca/apps/guessnumber/WelcomeActivity.java
+++ app/src/main/java/tw/org/tcca/apps/guessnumber/WelcomeActivity.java
@@ -2,7 +2,10 @@ package tw.org.tcca.apps.guessnumber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void gotoMain(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
